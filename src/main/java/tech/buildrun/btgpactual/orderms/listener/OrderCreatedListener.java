0d06129c631990bf1ca4b17ea5ffebf059d989c4
Carlos <carlos_eduardo_a.r@outlook.com>
2024-06-26 21:38:30 -0300

    feat: criacao da funcionalidade de cadastro de Order no MongoDB

--- src/main/java/tech/buildrun/btgpactual/orderms/listener/OrderCreatedListener.java
+++ src/main/java/tech/buildrun/btgpactual/orderms/listener/OrderCreatedListener.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 import tech.buildrun.btgpactual.orderms.listener.dto.OrderCreatedEvent;
+import tech.buildrun.btgpactual.orderms.service.OrderService;
 
 import static tech.buildrun.btgpactual.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
 
@@ -13,9 +14,16 @@ import static tech.buildrun.btgpactual.orderms.config.RabbitMqConfig.ORDER_CREAT
 public class OrderCreatedListener {
 
     private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
-    //Consumindo ORDER_CREATED_QUEUE
+
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService){
+        this.orderService=orderService;
+    }
+
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message){
         logger.info("Messagem consumed: {}", message);
+        orderService.save(message.getPayload());
     }
 }
---
+++ src/main/java/tech/buildrun/btgpactual/orderms/service/OrderService.java
@@ -0,0 +1,52 @@
+package tech.buildrun.btgpactual.orderms.service;
+
+import org.springframework.stereotype.Service;
+import tech.buildrun.btgpactual.orderms.entity.OrderEntity;
+import tech.buildrun.btgpactual.orderms.entity.OrderItem;
+import tech.buildrun.btgpactual.orderms.listener.dto.OrderCreatedEvent;
+import tech.buildrun.btgpactual.orderms.repository.OrderRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void save(OrderCreatedEvent event){
+        var entity = new OrderEntity();
+        entity.setOrderId(event.codigoPedido());
+        entity.setCustomerId(event.codigoCliente());
+        entity.setItems(getOrderItems(event));
+        entity.setTotal(getTotal(event));
+
+        orderRepository.save(entity);
+    }
+
+    /**
+     * Método para multiplicar o preco para a quantidade,
+     * acumular em um resultado final e caso não tenha nada
+     * returna 0. Sendo assim, todos os itens da entity serão
+     * setados.
+     * @param event
+     * @return
+     */
+    private BigDecimal getTotal(OrderCreatedEvent event) {
+        return event.itens().stream().map(i -> i.preco().multiply(BigDecimal.valueOf(i.quantidade()))).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+    }
+
+    /**
+     * Coletando os itens vindo do evento, convertendo através
+     * do map para o OrdemItem e por fim colocando na lista
+     * @param event
+     * @return
+     */
+    private static List<OrderItem> getOrderItems(OrderCreatedEvent event) {
+        return event.itens().stream().map(i -> new OrderItem(i.produto(), i.quantidade(), i.preco())).toList();
+    }
+}
